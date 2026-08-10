c3efd705613f1e091198c1491a598c7f8471defc
Rainer <rainerseventeen@outlook.com>
2026-08-10 17:12:21 +0800

    feat: lr_schedule, grad clip

--- courses/gpt_from_scratch/90_assignment_1/cs336_basics/optimizer.py
+++ courses/gpt_from_scratch/90_assignment_1/cs336_basics/optimizer.py
@@ -64,3 +64,33 @@ class AdamW(torch.optim.Optimizer):
                 state["t"] = t + 1
 
         return loss
+
+
+def get_lr_cosine_schedule(
+    it: int,
+    max_learning_rate: float,
+    min_learning_rate: float,
+    warmup_iters: int,
+    cosine_cycle_iters: int,
+):
+    if it < warmup_iters:
+        return max_learning_rate * it / warmup_iters
+    elif warmup_iters <= it <= cosine_cycle_iters:
+        cos_num = math.cos((it - warmup_iters) * math.pi / (cosine_cycle_iters - warmup_iters))
+        return min_learning_rate + 0.5 * (1 + cos_num) * (max_learning_rate - min_learning_rate)
+    else:
+        return min_learning_rate
+
+
+def gradient_clipping(
+    parameters: Iterable[torch.nn.Parameter], 
+    max_l2_norm: float,
+    eps: float = 1e-6,
+):
+    # 注意这里是全局的 l2 范数
+    total_norm = math.sqrt(sum(p.grad.square().sum() 
+                            for p in parameters if p.grad is not None))
+    clip_coef = max_l2_norm / (total_norm + 1e-6)
+    if clip_coef < 1:
+        grad *= clip_coef
+
